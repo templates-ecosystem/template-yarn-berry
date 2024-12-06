1aeb68759693baf14219b549405057888e57dc79
rtritto <ariele.1994@gmail.com>
2024-12-06 13:50:56 +0100

    Repro #3729

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,25 @@
-export const log = (msg: string) => console.log(msg)
+import { readFileSync } from 'node:fs'
+import { serve } from '@hono/node-server'
+import { Hono } from 'hono'
+
+const app = new Hono()
+
+const ZIP_NAME = '<FILENAME>.zip'
+const ZIP_FILEPATH = '/' + ZIP_NAME
+
+app.get('/downloadZip', (c) => {
+  c.header('Content-Disposition', `attachment; filename="${ZIP_NAME}"`)
+  c.header('Content-Type', 'application/zip-compressed')
+
+  return c.body(readFileSync(ZIP_FILEPATH))
+})
+
+const port = 3000
+
+serve(
+  {
+    fetch: app.fetch,
+    port
+  },
+  () => console.log(`Server running at http://localhost:${port}`)
+)
